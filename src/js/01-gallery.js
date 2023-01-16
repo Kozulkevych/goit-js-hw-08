8a1fee820066f1895f7d7f66cc1378300b6d607b
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-01-16 09:11:35 +0200

    task solution

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+localStorageInput();
+const formData = {};
+// console.log(formData);
+
+function onFormInput(e) {
+  formData[e.target.name] = [e.target.value];
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  // console.log(JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+
+  console.log('Send form', formData);
+}
+
+function localStorageInput() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
+  console.log(savedData);
+  if (parsedData) {
+    console.log(parsedData);
+    textarea.value = parsedData.message;
+    input.value = parsedData.email;
+  }
+}
+
+// textarea.addEventListener('input', throttle(onTextareaInput, 500));
+// function onEmailInput(e) {
+//     const email = e.target.value;
+//     console.log(email);
+//     localStorage.setItem(STORAGE_KEY, email);
+// }
+
+// function onTextareaInput(e) {
+//     const message = e.target.value;
+//     console.log(message);
+//     localStorage.setItem(STORAGE_KEY, message)
+// }
